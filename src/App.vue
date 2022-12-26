3b8b3e598f2630f5b2a2255cc73cc8cc7a6d2aad
ZifengChen <ken36chan@gmail.com>
2022-12-26 23:52:46 +0800

    vue学习d1

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div @click="parent">
+    <button @click.stop="xxx">点击我</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const parent = () => {
+  console.log('父级被点击')
+}
+const xxx = () => {
+  console.log('点击了xxx')
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
