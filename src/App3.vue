8b203d53fd22b44d66fac0dfa80794ab2ed21a87
Zifeng Chen <564520489@qq.com>
2022-12-28 17:50:14 +0800

    day3

--- src/App3.vue
+++ src/App3.vue
@@ -1,15 +1,35 @@
 <template>
-    <div style="display: flex;">
+    <div class="content">
+        <Diaglog>
+            <template v-slot:header>
+                <div>
+                    插入header
+                </div>
+            </template>
+            <template v-slot="{ data }">
+                <div>
+                    {{ data.name }} -- {{ data.age }}
+                </div>
+            </template>
+            <template v-slot:footer>
+                <div>
+                    插入footer
+                </div>
+            </template>
+        </Diaglog>
+    </div>
+    <!-- <div style="display: flex;">
         <div @click="switchCom(item, index)" :class="[activeId === index ? 'active' : '']" class="tabs"
             v-for="(item, index) in data">
             <div>{{ item.name }}</div>
         </div>
     </div>
-    <component :is="comId"></component>
+    <component :is="comId"></component> -->
 </template>
 
 <script setup lang='ts'>
 import { ref, reactive, shallowRef, markRaw } from 'vue'
+import Diaglog from './components/Dialog.vue'
 import AVue from './components/A.vue';
 import BVue from './components/B.vue';
 import CVue from './components/C.vue';
---
+++ src/components/Dialog.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <header class="header">
+            <slot name="header"></slot>
+        </header>
+        <main class="main">
+            <div v-for="item in data">
+                <slot :data="item"></slot>
+            </div>
+        </main>
+        <footer class="footer">
+            <slot name="footer"></slot>
+        </footer>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+type names = {
+    name: string,
+    age: number
+}
+
+const data = reactive([
+    {
+        name: 'ken1',
+        age: 18
+    },
+    {
+        name: 'ken2',
+        age: 19
+    },
+    {
+        name: 'ken3',
+        age: 20
+    },
+])
+
+</script>
+
+<style scoped lang="less">
+.header {
+    height: 200px;
+    background: red;
+    color: #fff;
+}
+
+.main {
+    height: 300px;
+    background: green;
+    color: #fff;
+}
+
+.footer {
+    height: 200px;
+    background: blue;
+    color: #fff;
+}
+</style>
